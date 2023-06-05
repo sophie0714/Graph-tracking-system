f7ce0fd4ac59686eefba5d6eeba677bd6d3154a3
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-06-05 21:11:52 +1200

    Delete length in the field

--- src/main/java/nz/ac/auckland/se281/datastructures/Queue.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Queue.java
@@ -1,28 +1,25 @@
 package nz.ac.auckland.se281.datastructures;
 
 public class Queue<T> {
-    private LinkedList<T> linkedList;
-    private int length;
+  private LinkedList<T> linkedList;
 
-    public Queue(){
-        linkedList= new LinkedList<T>();
-        length = 0;
-    }
+  public Queue() {
+    linkedList = new LinkedList<T>();
+  }
 
-    public void enQueue(T data){
-        linkedList.append(data);
-    }
+  public void enQueue(T data) {
+    linkedList.append(data);
+  }
 
-    public void deQueue(){
-        linkedList.remove(0);
-    }
+  public void deQueue() {
+    linkedList.remove(0);
+  }
 
-    public T Peek(){
-        return linkedList.fetch(0);
-    }
+  public T peek() {
+    return linkedList.fetch(0);
+  }
 
-    public boolean isEmpty(){
-        return length == 0;
-    }
-    
+  public boolean isEmpty() {
+    return linkedList.size() == 0;
+  }
 }
